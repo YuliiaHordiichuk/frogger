3bf82ffdedb437373d4e3e68c9e6f489588149e5
Yuliia <gordiychuk98@gmail.com>
2020-12-21 19:57:45 +0200

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,39 +1,111 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+const canvasWidth = 505;  
+const startX = 200; 
+const startY = 400; 
+const stepX = 100; 
+const stepY = 85; 
+const minX = 0; 
+const maxX = minX + 4 * stepX; 
+const minY = -25; 
+const maxY = startY;  
+const startEnemyX = -110 + Math.floor((Math.random()) * (-50));
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+const Enemy = function(x, y) {
+    this.x = x; 
+    this.y = y; 
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x += dt * 200; 
+    this.checkEnemyLocation(); 
+    this.checkCollision(); 
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+Enemy.prototype.checkEnemyLocation = function() {
+    if (this.x > canvasWidth) {
+        this.x = startEnemyX;  
+    }
+}; 
 
+Enemy.prototype.checkCollision = function() {
+    if(Math.abs(player.x - this.x) < 80 && this.y === player.y) {
+        setTimeout(() => {
+            alert ('You lost'); 
+            player.restart();
+        },0) 
+    };
+}; 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+const Player = function(x, y) {
+    this.x = x; 
+    this.y = y; 
+    this.sprite = 'images/char-boy.png';
+}
 
+Player.prototype.update = function() {
+    this.checkWin(); 
+}
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+Player.prototype.handleInput = function(key) {
+
+    switch(key) {
+        case 'left': {
+            const nextX = this.x - stepX;
+
+            if (nextX >= minX) {
+                this.x = nextX;
+            }
+            break; 
+        }
+        case 'up': {
+            const nextY = this.y - stepY; 
+
+            if (nextY >= minY) {
+                this.y = nextY;
+            }
+            break; 
+        }
+        case 'right': {
+            const nextX = this.x + stepX;
+            
+            if (nextX <= maxX) {
+                this.x = nextX; 
+            } 
+            break;
+        }
+        case 'down': {
+            const nextY = this.y + stepY;
+
+            if(nextY <= maxY) {
+                this.y = nextY;
+            }  
+            break;
+        } 
+    }
+}; 
+
+Player.prototype.restart = function() {
+    this.x = startX; 
+    this.y = startY; 
+}; 
+
+Player.prototype.checkWin = function() {
+    if (this.y === minY) {
+        setTimeout(() => {
+            alert('You won!'); 
+            this.restart(); 
+        }, 0)
+    }
+}; 
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -44,3 +116,13 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+const getEnemyY = (row) => startY - row * stepY; 
+
+const allEnemies = [
+    new Enemy(-150, getEnemyY(2)), 
+    new Enemy (-300, getEnemyY(3)), 
+    new Enemy (-50, getEnemyY(4))
+]; 
+
+const player = new Player(startX, startY); 
